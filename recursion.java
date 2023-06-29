2317f2c6395a1ab9f64a650b6df8a2a040fdb3a5
Sree Harsha M <98838137+sreeharsha243@users.noreply.github.com>
2023-06-29 22:05:19 +0530

    Add files via upload

---
+++ recursion.java
@@ -0,0 +1,18 @@
+import java.lang.*;
+
+public class recursion 
+{
+    static void fun(int n)
+    {
+        if(n>0)
+        {
+            System.out.println(n);
+            fun(n-1);
+        }
+    }
+    public static void main(String args[])
+    {
+        fun(4);
+    }
+    
+}
