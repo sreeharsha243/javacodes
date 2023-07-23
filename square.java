f5da2d977cb6956b6e7c71e1d223b2741616c54b
Sree Harsha M <98838137+sreeharsha243@users.noreply.github.com>
2023-07-23 23:44:27 +0530

    Add files via upload

---
+++ square.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class square
+{
+    public static void main(String args[])
+    {
+        int x=0;
+        double r=Math.sqrt(x);
+        System.out.println("the square of " + x + " is "+r);
+        if(x==0)
+        {
+            System.out.println("the square of zero is 0");
+        }
+    }
+}
