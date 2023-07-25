ba6c8ca2eb9f6c6f211a2891a67c2dfbc0e72960
Sree Harsha M <98838137+sreeharsha243@users.noreply.github.com>
2023-07-25 22:53:45 +0530

    Add files via upload

---
+++ third.java
@@ -0,0 +1,18 @@
+import java.lang.*;
+
+class third
+{
+ public static void main(String arg[])
+{
+byte n=100;
+int s=1000;
+short t=222;
+float a=99.9f;
+System.out.println(n);
+System.out.println(s);
+System.out.println(t);
+System.out.println(a);
+}
+
+
+}
