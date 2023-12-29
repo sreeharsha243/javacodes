a4b6ef6eabee05d24ea6215ca57568c41ed93a00
Sree Harsha M <98838137+sreeharsha243@users.noreply.github.com>
2023-12-30 01:03:05 +0530

    Add files via upload

---
+++ wrapper.java
@@ -0,0 +1,60 @@
+import java.lang.*;
+
+public class wrapper {
+    public static void main(String args[])
+    {
+        /*Integer i=new Integer(10);
+        Integer a=Integer.valueOf(10);
+        Integer b=10;
+
+        Byte c=12;
+        Byte d=Byte.valueOf("15");
+        byte bb=15;
+        Byte e=Byte.valueOf(bb); //boxing or wrapping
+
+        Short f=Short.valueOf("123");
+
+        Float g=21.3f;
+        Float h=Float.valueOf("123.5");
+
+        Double j =Double.valueOf(123.456);
+
+        Character k=Character.valueOf('A');
+
+        Boolean l=Boolean.valueOf("true");
+
+        float x=h.floatValue();
+        float y=h;
+
+        int m=10;// auto boxing
+        Integer p=Integer.valueOf(m);//boxing
+        int n=m;// auto unboxing */
+
+        /*int m=15;
+
+        Integer m1=Integer.valueOf("123");
+        Integer m2=Integer.valueOf("A7",16);
+        Integer m3=Integer.decode("0xA7");
+
+        System.out.println(Integer.toBinaryString(15));*/
+
+        /*float a=12.5f;
+        Float b=(float)Math.sqrt(9);
+
+        //System.out.println(b.isNaN());
+        System.out.println(b!=Float.NaN); //Don't use this for NaN*/
+
+
+        /*String s1=new String("hello");
+        StringBuffer s2=new StringBuffer("hello");
+        StringBuilder s3=new StringBuilder("hello");
+
+        s1.concat("world");
+        s2.append("world");
+        s3.append("world");
+
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3);*/
+    }
+}
