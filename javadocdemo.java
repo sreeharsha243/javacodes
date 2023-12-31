d35f22a0a90fd937bf8300e84a3b1dc039180d62
Sree Harsha M <98838137+sreeharsha243@users.noreply.github.com>
2023-12-31 21:15:12 +0530

    Add files via upload

---
+++ javadocdemo.java
@@ -0,0 +1,28 @@
+/**@author 91939
+ * @version 2.0
+ * @since 2023
+ **/
+package javadocdemo;
+public class javadocdemo
+{
+    /**
+     * @value 10 default value
+     */
+
+    static int val=10;
+
+    /**
+     * Parametical Constructor
+     * @param s Book Name
+     */
+    public javadocdemo(String s)
+    {
+
+    }
+
+    /**
+     * Issue a Book to a student
+     * @param roll roll number of a student
+     */
+
+}
