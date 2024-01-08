80532eb49a521df003a605df8226310a4b71167a
Sree Harsha M <98838137+sreeharsha243@users.noreply.github.com>
2024-01-08 23:20:19 +0530

    Add files via upload

---
+++ IOstream.java
@@ -0,0 +1,25 @@
+/* INPUT STREAM
+  int read()
+* int read(byte[] b)
+* int read(byte[] b,int off,int len)
+*
+* int available()
+* int skip(long n)
+*
+* void mark(int limit)
+* void reset()
+* boolean markSupported()
+* void close()
+
+  OUTPUT STREAM
+  void write(int b)
+  void write(byte[] b)
+  void write(byte[] b,int off,int len)
+
+  void flush()
+  void close()*/
+
+public class IOstream
+{
+
+}
