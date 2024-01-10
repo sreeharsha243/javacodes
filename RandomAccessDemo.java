d24ce44bb4333730841721a8222c6b4cf17b10a4
Sree Harsha M <98838137+sreeharsha243@users.noreply.github.com>
2024-01-10 20:08:22 +0530

    Add files via upload

---
+++ RandomAccessDemo.java
@@ -0,0 +1,22 @@
+import java.io.*;
+
+public class RandomAccessDemo
+{
+    public static void main(String args[])throws Exception
+    {
+        RandomAccessFile rf=new RandomAccessFile("C:/Users/91939/Java/test.txt","rw");
+
+        System.out.println((char)rf.read());
+        System.out.println((char)rf.read());
+        System.out.println((char)rf.read());
+        rf.write('d');
+        System.out.println((char)rf.read());
+        rf.skipBytes(3);
+        System.out.println((char)rf.read());
+        rf.seek(3);
+        System.out.println((char)rf.read());
+        System.out.println(rf.getFilePointer());
+        rf.seek(rf.getFilePointer()+2);
+        System.out.println((char)rf.read());
+    }
+}
